034cabc8e86522a4616df58bc4e4ea4406c778d0
Nithish <nithishm612345@gmail.com>
2024-06-18 22:22:30 +0530

    first commit

---
+++ apps/client-react/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v22.3.0/bin
+
+cd /home/ubuntu/monorepo-full
+git pull origin main
+npm run build
+pm2 stop react
+pm2 start npm --name "react" -- run "start:react"
---
+++ apps/express-backend/deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v22.3.0/bin
+
+cd /home/ubuntu/monorepo-full
+git pull origin main
+npm run build
+pm2 stop express
+pm2 start npm --name "express" -- run "start:express"
