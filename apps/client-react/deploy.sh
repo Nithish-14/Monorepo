12a8bbb257a2dfbdf304b4ee347fcca16cd9da39
Nithish <nithishm612345@gmail.com>
2024-06-18 22:26:15 +0530

    change repo name

--- apps/client-react/deploy.sh
+++ apps/client-react/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v22.3.0/bin
 
-cd /home/ubuntu/monorepo-full
+cd /home/ubuntu/Monorepo-Deploy
 git pull origin main
 npm run build
 pm2 stop react
